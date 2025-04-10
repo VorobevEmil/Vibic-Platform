7899f803c28856869afbf5cbb54a67138ca5edc9
EmilVorobev <vorobyovemil@gmail.com>
2025-04-11 00:24:27 +0200

    feat: Cleared MediaService

--- scr/MediaService/scr/MediaService.Application/DTOs/ParticipantRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace MediaService.Application.DTOs;
-
-public class ParticipantRequest
-{
-    public Guid UserId { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public bool IsMuted { get; set; }
-}
--- scr/MediaService/scr/MediaService.Application/DTOs/VoiceSessionRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace MediaService.Application.DTOs;
-
-public class VoiceSessionRequest
-{
-    public Guid SessionId { get; set; }
-    public Guid ChannelId { get; set; }
-    public Guid OwnerId { get; set; }
-    public List<ParticipantRequest> Participants { get; set; } = new();
-}
